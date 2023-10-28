f04b5a83ba41aabf5a882aaa3daf938d7e64b20e
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-10-29 00:57:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-image_encoder_sd15_path = "/content/models/image_encoder/pytorch_model.bin"
-ipadapter_sd15_path = "/content/models/ip-adapter_sd15.bin"
+image_encoder_sd15_path = "/content/Face_Int_ip/models/image_encoder/pytorch_model.bin"
+ipadapter_sd15_path = "/content/Face_Int_ip/models/ip-adapter_sd15.bin"
